5428b3e7b5e9e9254c3cf1def677f93b7a4400b8
rjcabascangoa <159057498+rjcabascangoa@users.noreply.github.com>
2025-08-23 20:36:18 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -4,7 +4,25 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+       #return '¡Hola! Bienvenido a mi aplicación Flask.'
+    return render_template('index.html', title='Inicio')
+
+
+
+@app.route('/usuario/<nombre>')
+def usuario(nombre):
+    return f'Bienvenido, {nombre}!'
+
+@app.route('/about/')
+def about():
+    return render_template('about.html', title='Acerca de')
+
+@app.route('/contactos')
+def contactos():
+    return"Contacta con nosotros"
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
